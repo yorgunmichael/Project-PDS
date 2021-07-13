bbdc88775c4308a2e4162d673aa189d51b733921
yorgunmichael <yorgunchacha@yahoo.fr>
2021-07-13 14:15:42 +0200

    creat script of backend-service

---
+++ backend-service/src/main/java/episen/backend/server/BackendService.java
@@ -0,0 +1,13 @@
+package episen.backend.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BackendService {
+
+    private static final Logger logger = LoggerFactory.getLogger(BackendService.class.getName());
+
+    public static void main(String[] args) {
+        logger.info("BackendService is running");
+    }
+}
