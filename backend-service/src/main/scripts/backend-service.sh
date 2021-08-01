3387bb2e5ca9ee3798479398a4232e31c7f024fe
yorgunmichael <yorgunchacha@yahoo.fr>
2021-08-01 10:58:27 +0200

    update pom of backend-service and client-service

--- backend-service/src/main/scripts/backend-service.sh
+++ backend-service/src/main/scripts/backend-service.sh
@@ -1,5 +1,2 @@
-m2=${M2_REPO}
-clsspath=src/main/resources
-clsspath+=${clsspath}:${m2}/episen/backend-service/1.0-SNAPSHOT/backend-service-1.0-SNAPSHOT-jar-with-dependencies.jar
-exec java -cp ${clsspath} episen.backend.server.BackendService $*
-
+#exec java -cp /home/tata/tests/backend-service-1.0-SNAPSHOT-jar-with-dependencies.jar episen.backend.server.BackendService $*
+exec java -cp /home/tata/tests/backend-service-1.0-SNAPSHOT-jar-with-dependencies.jar episen.backend.server.BackendService $*
