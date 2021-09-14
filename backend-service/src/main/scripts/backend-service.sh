c4f06a4811d2aa9055ccdc8a92b843443ce5d071
yorgunmichael <yorgunchacha@yahoo.fr>
2021-09-14 21:01:19 +0200

    script and deployment

--- backend-service/src/main/scripts/backend-service.sh
+++ backend-service/src/main/scripts/backend-service.sh
@@ -1,2 +1,2 @@
 #exec java -cp /home/tata/tests/backend-service-1.0-SNAPSHOT-jar-with-dependencies.jar episen.backend.server.BackendService $*
-exec java -cp /home/tata/tests/backend-service-1.0-SNAPSHOT-jar-with-dependencies.jar episen.backend.server.BackendService $*
+exec java -cp /usr/local/newera/jar/backend-service-1.0-SNAPSHOT-jar-with-dependencies.jar episen.backend.server.BackendService $*
--- scripts/deploybackend.bat
+++ scripts/deploybackend.bat
@@ -1,7 +1,7 @@
 set m2=C:/Users/yorgu/.m2/repository/episen/backend-service/1.0-SNAPSHOT/backend-service-1.0-SNAPSHOT-jar-with-dependencies.jar
 set project=C:/Users/yorgu/IdeaProjects/Project-PDS/backend-service/src/main/scripts/backend-service.sh
-scp %m2% tata@172.31.249.254:/home/tata/tests/
-scp %project% tata@172.31.249.254:/home/tata/tests/
-ssh tata@172.31.249.254 chmod 777 /home/tata/tests/backend-service.sh
+scp %m2% admin@172.31.249.254:/usr/local/newera/jar
+scp %project% admin@172.31.249.254:/usr/local/newera/script
+ssh admin@172.31.249.254 chmod 777 /usr/local/newera/script/backend-service.sh
 
 
