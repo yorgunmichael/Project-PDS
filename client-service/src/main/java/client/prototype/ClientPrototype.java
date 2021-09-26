97b88ed362cf03476e317b47fe914f9dffaa7603
yorgunmichael <yorgunchacha@yahoo.fr>
2021-09-26 15:33:44 +0200

    correction of bugs finished

--- client-service/src/main/java/client/prototype/ClientPrototype.java
+++ client-service/src/main/java/client/prototype/ClientPrototype.java
@@ -28,14 +28,29 @@ public class ClientPrototype {
         try {
             final Options options = new Options();
             final Option student = Option.builder().longOpt("student").hasArg().argName("student").build();
+            final Option students = Option.builder().longOpt("students").hasArg().argName("students").build();
             final Option crud_insert = Option.builder().longOpt("crud").hasArg().argName("crud").build();
             options.addOption(student);
+            options.addOption(students);
             options.addOption(crud_insert);
             final CommandLineParser commandLineParser = new DefaultParser();
             final CommandLine commandLine = commandLineParser.parse(options, args);
 
             if (commandLine.hasOption("student")) {
                 String request = commandLine.getOptionValue("student");
+                String crud_op = commandLine.getOptionValue("crud");
+                log.info(request);
+                log.info(crud_op);
+                configuration = System.getenv(configurationvariable);
+                String values = Files.readString(Path.of(configuration));
+                ObjectMapper jmapper = new ObjectMapper(new JsonFactory());
+
+                map = jmapper.readValue(values, new TypeReference<Map<String, Map<String, String>>>() {
+                });
+                getSendd(request, crud_op);
+            }
+            if(commandLine.hasOption("students")) {
+                String request = commandLine.getOptionValue("students");
                 log.info(request);
                 configuration = System.getenv(configurationvariable);
                 String values = Files.readString(Path.of(configuration));
@@ -44,54 +59,11 @@ public class ClientPrototype {
                 map = jmapper.readValue(values, new TypeReference<Map<String, Map<String, String>>>() {
                 });
                 getSend(request);
-            } else if (commandLine.hasOption("student") & commandLine.hasOption("crud")) {
-                String request = commandLine.getOptionValue("student");
-                String crud_op = commandLine.getOptionValue("crud");
-                log.info(request);
-                log.info(crud_op);
-                configuration = System.getenv(configurationvariable);
-                String values = Files.readString(Path.of(configuration));
-                ObjectMapper jmapper = new ObjectMapper(new JsonFactory());
-                map = jmapper.readValue(values, new TypeReference<Map<String, Map<String, String>>>() {
-                });
-                getSendd(request, crud_op);
-            } else {
-                log.info("missing student argument");
+
             }
-
-
-
         } catch (IOException | ParseException e) {
             e.printStackTrace();
         }
-    }
-
-    public static String getSend(String request) {
-        String answer = null;
-        try {
-            Socket socket = new Socket(new ClientConfiguration().getConfiguration().getAdressIP(), new ClientConfiguration().getConfiguration().getPort());
-            InputStream in = socket.getInputStream();
-            OutputStream out = socket.getOutputStream();
-            ObjectMapper objectMapper = new ObjectMapper();
-            String data = objectMapper.writeValueAsString(map.get(request));
-            DataInputStream inputData = new DataInputStream(in);
-            DataOutputStream outputData = new DataOutputStream(out);
-            log.info(request);
-            log.info(data);
-
-            outputData.writeUTF(request + "@" + data);
-            try {
-                sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            answer = inputData.readUTF();
-            log.info(answer);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return answer;
     }
 
     public static String getSendd(String request, String crud_op) {
@@ -115,6 +87,34 @@ public class ClientPrototype {
             outputData.writeUTF(request + "@" + data);
             try {
                 sleep(2500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            answer = inputData.readUTF();
+            log.info(answer);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return answer;
+    }
+
+    public static String getSend(String request) {
+        String answer = null;
+        try {
+            Socket socket = new Socket(new ClientConfiguration().getConfiguration().getAdressIP(), new ClientConfiguration().getConfiguration().getPort());
+            InputStream in = socket.getInputStream();
+            OutputStream out = socket.getOutputStream();
+            ObjectMapper objectMapper = new ObjectMapper();
+            String data = objectMapper.writeValueAsString(map.get(request));
+            DataInputStream inputData = new DataInputStream(in);
+            DataOutputStream outputData = new DataOutputStream(out);
+            log.info(request);
+            log.info(data);
+
+            outputData.writeUTF(request + "@" + data);
+            try {
+                sleep(500);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
